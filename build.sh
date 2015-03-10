2eb1123b59d952f4e2be3f6d28dda92de6e6b621
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-03-10 15:42:24 +0200

    move to paket

--- build.sh
+++ build.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+if [ "X$OS" = "XWindows_NT" ] ; then
+  # use .Net
 
-if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
-  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  .Paket/Paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  .Paket/Paket.exe restore -v
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+else
+
+  # use mono
+  mono .Paket/Paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  mono .Paket/Paket.exe restore -v
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
 fi
-
-mono packages/FAKE/tools/FAKE.exe build.fsx $@
